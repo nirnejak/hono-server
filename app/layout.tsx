8c78dd85348b513d631d1b926b3bda937088b519
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-20 23:17:55 +0530

    Code Formatted

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>{children}</body>
